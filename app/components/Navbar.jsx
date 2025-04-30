bfd04757d3894fac9efa008853cc62043dad4303
Saurabh-2607 <saurabh.nayla@gmail.com>
2025-04-30 23:51:34 +0530

    fix: Correct import path for FullscreenMenu component to FullScreenMenu

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Menu, Search } from "lucide-react";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
-import FullscreenMenu from "./FullscreenMenu";
+import FullscreenMenu from "./FullScreenMenu";
 import Newsletter from "./Newsletter";
 
 const Navbar = () => {
